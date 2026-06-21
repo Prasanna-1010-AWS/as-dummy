79aa6299ca898167a484a6505c4d20e4b8942727
Prasanna Meenamanalan <prasanna.m.loa@gmail.com>
2026-06-22 00:58:35 +0530

    Delete src/frontend/__tests__ directory

--- src/frontend/__tests__/sample.test.js
+++
@@ -1,9 +0,0 @@
-describe("Basic CI sanity test", () => {
-  test("adds numbers correctly", () => {
-    expect(2 + 2).toBe(4);
-  });
-
-  test("string works", () => {
-    expect("devops").toContain("dev");
-  });
-});
