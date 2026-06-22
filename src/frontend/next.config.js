57cd5e635f245f1141ad08bc5f1fd222983915b0
Prasanna Meenamanalan <prasanna.m.loa@gmail.com>
2026-06-23 02:02:23 +0530

    Refactor dotenv variable names and update config

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -3,15 +3,14 @@
 
 /** @type {import('next').NextConfig} */
 
-const dotenv = require('dotenv');
+const dotEnv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 const { resolve } = require('path');
 
-const myEnv = dotenv.config({
+const myEnv = dotEnv.config({
   path: resolve(__dirname, '../../.env'),
 });
-
-dotenvExpand.expand(myEnv || { parsed: {} });
+dotenvExpand.expand(myEnv);
 
 const {
   AD_ADDR = '',
@@ -24,31 +23,27 @@ const {
   ENV_PLATFORM = '',
   OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '',
   OTEL_SERVICE_NAME = 'frontend',
-  PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = ''
+  PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '',
 } = process.env;
 
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-
+  swcMinify: true,
   compiler: {
-    styledComponents: true
+    styledComponents: true,
   },
-
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback = {
-        http2: false,
-        tls: false,
-        net: false,
-        dns: false,
-        fs: false
-      };
+      config.resolve.fallback.http2 = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.dns = false;
+      config.resolve.fallback.fs = false;
     }
 
     return config;
   },
-
   env: {
     AD_ADDR,
     CART_ADDR,
@@ -57,17 +52,14 @@ const nextConfig = {
     PRODUCT_CATALOG_ADDR,
     RECOMMENDATION_ADDR,
     SHIPPING_ADDR,
-
     OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
-
     NEXT_PUBLIC_PLATFORM: ENV_PLATFORM,
     NEXT_PUBLIC_OTEL_SERVICE_NAME: OTEL_SERVICE_NAME,
-    NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT: PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT
+    NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT: PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
   },
-
   images: {
-    loader: 'custom',
-    loaderFile: './utils/imageLoader.js'
+    loader: "custom",
+    loaderFile: "./utils/imageLoader.js"
   }
 };
 
